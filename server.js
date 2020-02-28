a4659ecdf6fb7bfb356c9eff99cbb5e270329477
Gabriel Todirenchi <gabriel.todirenchi@softvision.ro>
2020-02-28 17:51:00 +0200

    Add request

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 const request = require("request");
 const config = require("./config");
 
 const url = `https://api.darksky.net/forecast/${config.apiKey}/47.151726,27.587914`;
 
-
-request({ url: url }, () => {});
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.currently);
+});
