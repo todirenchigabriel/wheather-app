38d08af2df7922e6c3b5cc46e24e446d6186d8aa
Gabriel Todirenchi <gabriel.todirenchi@softvision.ro>
2020-02-28 14:55:37 +0200

    Remove secrets

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    apiKey: '1aa2c731b18c587be675a15714f77f71'
+    apiKey: process.env.apiKey
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const request = require('request');
-const config = require('./config');
+require('dotenv').config();
+const request = require("request");
+const config = require("./config");
 
 const url = `https://api.darksky.net/forecast/${config.apiKey}/47.151726,27.587914`;
 
-request({url: url}, () => {
 
-})
+request({ url: url }, () => {});
