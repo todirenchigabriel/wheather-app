36671e0e2261cc46f938d0898e091db524bc50b8
Gabriel Todirenchi <gabriel.todirenchi@softvision.ro>
2020-03-09 11:59:04 +0200

    Add lecture 32

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const request = require("request");
 const config = require("./config");
 
-const url = `https://api.darksky.net/forecast/${config.apiKey}/47.151726,27.587914`;
+const url = `https://api.darksky.net/forecast/${config.apiKey}/47.151726,27.587914?units=si&lang=ro`;
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  const { temperature, precipProbability } = response.body.currently;
+  const { summary } = response.body.daily.data[0];
+  console.log(
+    `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability *
+      100}% change of rain`
+  );
 });
